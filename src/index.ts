5fd709993f10bd7a6e0b9b6d263d9f91cd391fc0
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-06-02 19:51:22 -0300

    commit02/06 Corregí el ejercicio luego de la devolución del tutor.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,18 @@
-/*let num1: number = Number(prompt("Ingrese el dividendo"));
-let num2: number = Number(prompt("Ingrese el divisor"));
+let numDivisores: number = Number(prompt("Ingrese un número entero"));
+let resultado: number = 0;
 
-function esMultiplo(num1: number, num2: number): boolean {
-  let verdadero: boolean = true;
-  let falso: boolean = false;
-  let resultado: boolean;
-  if (num1 % num2 === 0) {
-    resultado = verdadero;
-  } else {
-    resultado = falso;
-  }
-  return resultado;
-}
-
-console.log("El resultado es: ", esMultiplo(num1, num2));*/
-
-let numEntero: number = Number(prompt("Ingrese un número entero"));
-
-function cantidadDeDivisores(numEntero: number): number {
-  let cantidad: number = 0;
-  let divisor: number;
-  for (divisor = 0; divisor <= numEntero; divisor++) {
-    if (esMultiplo(num1, num2)) {
-      cantidad++;
-    } else {
+function cantidadDeDivisores(valor: number): number {
+  let contador: number = 0;
+  for (let i: number = 1; i <= valor; i++) {
+    if (valor % i === 0) {
+      contador++;
+      console.log(contador);
     }
   }
-  return cantidad;
+  return contador;
 }
 
-let cantidadDivisores: number = cantidadDeDivisores(numEntero);
+resultado = cantidadDeDivisores(numDivisores);
 console.log(
-  `La cantidad de divisores del número ${numEntero} es: ${cantidadDivisores}`
+  `La cantidad de divisores del número ${numDivisores} es: ${resultado}`
 );
